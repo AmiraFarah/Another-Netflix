5f9990497c4545b6ad006d6c347a7446b5dcd2e1
amira farah <amirasaeedmoh@gmail.com>
2022-06-14 15:19:42 -0500

    created folders structures and  user & movie models

---
+++ server.js
@@ -0,0 +1,13 @@
+// Imports
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+//==================ROUTES============
+
+
+
+// ===== PORT =====
+const port = 8080
+
+app.listen(port, () => console.log(`Express app running on port ${port}`))
