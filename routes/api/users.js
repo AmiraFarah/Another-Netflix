8bb8f9760ebac0c6ecba0c9d950cc214db6ece38
amirafarah <amirasaeedmoh@gmail.com>
2022-06-17 09:07:32 -0500

    auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const usersCtrl = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 // POST /api/v1/users
 router.post('/', usersCtrl.create)
 // POST /api/v1/users/login
-// router.post('/login', usersCtrl.login)
+router.post('/login', usersCtrl.login)
 // GET /api/v1/users/:id/favorites
-router.get('/:id/favorites', usersCtrl.getFavorites)
+router.get('/:id/favorites',ensureLoggedIn, usersCtrl.getFavorites)
 // GET /api/v1/users/:id
-router.get('/:id', usersCtrl.show)
+router.get('/:id',ensureLoggedIn, usersCtrl.show)
 
 // PUT /api/v1/users/:id
-router.put('/:id', usersCtrl.update)
+router.put('/:id',ensureLoggedIn, usersCtrl.update)
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,11 +7,20 @@ require('./config/database')
 // ===== MIDDLEWARES =====
 app.use(express.json())
 
+// check if we have a token and create req.user
+// app.use(require('./config/checkToken'))
+
 // ===== ROUTES =====
-// Movies
-app.use('/api/v1/movies', require('./routes/api/movies'))
+
 // Users
 app.use('/api/v1/users', require('./routes/api/users'))
+
+// PROTECT API routes below from  unATHORIZED USERS
+const ensureLoggedIn = require('./config/ensureLoggedIn') 
+// Movies
+app.use('/api/v1/movies', ensureLoggedIn, require('./routes/api/movies'))
+
+
 
 // ===== PORT =====
 const port = 8080
